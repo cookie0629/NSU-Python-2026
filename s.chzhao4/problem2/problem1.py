6d231935fea8630ef49e9ebe62b5f68757f225b1
Cookie <2389397239@qq.com>
2026-03-24 19:01:31 +0700

    zhaosongtaoP2p1

--- s.chzhao4/problem2/problem1.py
+++ s.chzhao4/problem2/problem1.py
@@ -1,11 +1,18 @@
-n = 30
+def generate_pythagorean_triples(n: int) -> list:
+    if n < 5:
+        return []
 
-pythagorean_triples = [
-    (x, y, z)
-    for x in range(1, n + 1)
-    for y in range(x, n + 1)
-    for z in range(y, n + 1)
-    if x**2 + y**2 == z**2
-]
+    return [(x, y, z)
+            for x in range(1, n)
+            for y in range(x + 1, n)
+            for z in range(y + 1, n + 1)
+            if x**2 + y**2 == z**2]
 
-print(f"n={n} 以内的勾股数: {pythagorean_triples}")
+if __name__ == "__main__":
+    try:
+        user_input = int(input("Please enter the maximum range n:"))
+        triples = generate_pythagorean_triples(user_input)
+        for t in triples:
+            print(t)
+    except ValueError:
+        print("Invalid input:")
