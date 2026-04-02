1d006bd0b0270afaa2d6e5be66f958a61de653e2
Cookie <2389397239@qq.com>
2026-04-02 13:04:32 +0700

    zhaosongtaoP1p4

--- s.chzhao4/problem1/problem4.py
+++ s.chzhao4/problem1/problem4.py
@@ -6,6 +6,8 @@ def generate_ten_green_bottles() -> list:
 
     lyrics = []
 
+    fall_phrase = " one green bottle should accidentally fall,"
+
     for i in range(10, 0, -1):
         current_num_word = numbers[i]
         next_num_word = numbers[i - 1].lower()
@@ -19,9 +21,9 @@ def generate_ten_green_bottles() -> list:
         lyrics.append(line)
 
         if i == 1:
-            lyrics.append("If that one green bottle should accidentally fall,")
+            lyrics.append(f"If that{fall_phrase}")
         else:
-            lyrics.append("And if one green bottle should accidentally fall,")
+            lyrics.append(f"And if{fall_phrase}")
 
         lyrics.append(f"There'll be {next_num_word} green {next_bottle_str} hanging on the wall.")
 
@@ -32,6 +34,7 @@ def sing_ten_green_bottles() -> None:
     lyrics_lines = generate_ten_green_bottles()
     for line in lyrics_lines:
         print(line)
+
 
 if __name__ == "__main__":
     sing_ten_green_bottles()
